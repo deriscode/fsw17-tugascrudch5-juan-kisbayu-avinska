8d0935383627e62acacdd3a20b87731208a8266d
avinska syaviendy <avinskat@gmail.com>
2022-01-20 20:51:07 +0700

    add crud operations and json initial file

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
 const express = require("express");
 const app = express();
+const { v4:uuid } = require("uuid")
+const fs = require("fs")
+const methodOverride = require('method-override')
+const bcrypt = require('bcrypt')
 
 app.set("view engine", "ejs");
 app.set("views", "./public/views");
+app.use(methodOverride('_method'))
 app.use(express.static(__dirname + "/public/"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.render("main", {});
-});
+const data = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'))
+
+app.get("/user", (req, res) => {
+	res.render("main.ejs", {users:data})
+})
+
+app.get('/user/new', (req, res) => {
+	res.render('new.ejs')
+})
+
+app.post('/user', async (req, res) => {
+	const {name, email, password} = req.body
+	const hashedPass = await bcrypt.hash(password, 10)
+	const newUser = {id: uuid(), name, email, password: hashedPass}
+	data.push(newUser)
+	fs.writeFileSync('./data/users.json', JSON.stringify(data, null, 4))
+	res.redirect('/user')
+})
+
+app.get('/user/:id/edit', (req, res) => {
+	const {id} = req.params
+	const foundUser = data.find(user => user.id === id)
+	res.render('edit.ejs', {user : foundUser})
+})
+
+app.put('/user/:id', (req, res) => {
+	const {id} = req.params
+	const {name, email, password} = req.body
+	const editedUser = {id, name, email, password}
+	const foundUserIndex = data.findIndex(user => user.id === id)
+	data[foundUserIndex] = editedUser
+	fs.writeFileSync('./data/users.json', JSON.stringify(data, null, 4))
+	res.redirect('/user')
+})
 
 const PORT = 5000;
 app.listen(PORT, () => {
